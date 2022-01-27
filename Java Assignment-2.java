03613fc625a241c7e97c17224a54849eb3df7800
amandeep267 <amandeep.kaur@zemosolabs.com>
2022-01-27 12:07:06 +0530

    java file created

--- Java Assignment-2.java
+++ Java Assignment-2.java
@@ -1,16 +1,17 @@
 public class Alphabets {
   public static void main(String[] args) {
-   //Space complexity - O(1)
-  //Time complexity - O(n) 
-   String string="abcdefGHIJKL__mnOPQRstuvwxyZ";
+   // Space Complexity - O(1)
+   // Time Complexity - O(n)
+   String string="abcdefGHIJKL__mPQRstuvwxyZ";
    boolean visited[]=new boolean[26];
    
    if(checkAllTheLetters(string,visited))
     System.out.println("True");
    else
    System.out.println("False");  
-   
+ 
     }
+    
     static boolean checkAllTheLetters(String string,boolean visited[])
     {
      int tempIndex=0;
@@ -18,23 +19,23 @@ public class Alphabets {
     for(int index=0;index<string.length();index++)
     {
         if(string.charAt(index)>='A'&&string.charAt(index)<='Z')
-        {
          tempIndex=string.charAt(index)-'A';
-        }
+        
         else if(string.charAt(index)>='a'&&string.charAt(index)<='z')
-        {
         tempIndex=string.charAt(index)-'a';
-        }
+        
         visited[tempIndex]=true;
     }
-    
-    for(int index=0;index<visited.length;index++)
-    {
-      if(visited[index]==false)
-      {return false;}
+    return checkIfAllAlphabetsAreVisited(visited);   
     }
+
+   static boolean checkIfAllAlphabetsAreVisited(boolean visited[])
+    {
+    for(int index=0;index<visited.length;index++)    
+      if(visited[index]==false)
+        return false;
+
     return true;
     }
-    
   
 }
